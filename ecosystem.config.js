a3968c740109a67e4a2f7547f0dd3fa0cc592501
Gerald Michelant <gmichelant@Gerald-Michelants-MacBook-Pro.local>
2018-10-26 00:39:37 -0400

    added post-deploy to pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,10 +11,11 @@ module.exports = {
       key: "/Users/gmichelant/.ssh/id_rsa",
       user : 'pi',
       host : '192.168.2.28',
+      ssh_options: "StrictHostKeyChecking=no",
       ref  : 'origin/master',
       repo : 'https://GeraldM1614@bitbucket.org/gegeraptor/weather-client.git',
       path : '/home/pi/weather',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      post-deploy : 'npm install && pm2 reload ecosystem.config.js'
     }
   }
 };
